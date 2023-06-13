4ae82069e8122cf3502f5b3c0d3924c7a6dc9916
Yogesh1101 <114221824+Yogesh1101@users.noreply.github.com>
2023-06-13 10:22:13 +0530

    Delete pulsOne.js

--- pulsOne.js
+++
@@ -1,9 +0,0 @@
-var arr = [1,2,2], len = arr.length, result = []
-var a = arr.join("")
-var temp = parseInt(a) + 1
-for(let i = 0; i < len; i++)
-{
-    result.unshift(temp%10)
-    temp = parseInt(temp / 10)
-}
-console.log(result)
